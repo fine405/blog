dec341d8f261a0cc12d174e8d863b5fefbd23a23
fine <18224017933@163.com>
2025-12-03 15:14:54 +0800

    ci: Add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-domain.vercel.app',
+  site: 'https://fine405.github.io',
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
